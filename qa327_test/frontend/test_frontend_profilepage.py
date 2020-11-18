1b8e1f2842feaeb93953191346caac5524a8ad8b
HamzaSherik <16hs46@queensu.ca>
2020-11-18 14:11:40 -0500

    small fixes to index.html. Added new test cases. Working on figuring out how to test R3.9 through R3.11

--- qa327_test/frontend/test_frontend_profilepage.py
+++ qa327_test/frontend/test_frontend_profilepage.py
@@ -187,4 +187,101 @@ class UserProfilePageTestFrontEnd(BaseCase):
         self.assert_text("Expiration Date", "#date")
         
     #test R3.7
+    @patch('qa327.backend.get_user', return_value=new_test_user)
+    def test_sell_form(self, *_):
         
+        # open /logout to invalidate any current logged in sessions
+        self.open(base_url + '/logout')
+        
+        #login page opens
+        self.open(base_url + '/login')
+        
+        #enter email and password of test_user into #email and #password elements
+        self.type("#email", "test_frontend@test.com")
+        self.type("#password", "test_Frontend0!")
+        
+        # click login button
+        self.click('input[type="submit"]')
+        
+        # open user profile page
+        self.open(base_url)
+        
+        #validate buy form exists
+        self.assert_element("#buy-header")
+        self.assert_text("Buy a Ticket!", "#buy-header")
+        
+        #validate buy form includes #name element with correct attached phrase
+        self.assert_element("#name")
+        self.assert_text("Name", "#name")
+        
+        #validate buy form includes #quantity element with correct attached phrase
+        self.assert_element("#quantity")
+        self.assert_text("Quantity", "#quantity")
+        
+    #test R3.7
+    @patch('qa327.backend.get_user', return_value=new_test_user)
+    def test_sell_form(self, *_):
+        
+        # open /logout to invalidate any current logged in sessions
+        self.open(base_url + '/logout')
+        
+        #login page opens
+        self.open(base_url + '/login')
+        
+        #enter email and password of test_user into #email and #password elements
+        self.type("#email", "test_frontend@test.com")
+        self.type("#password", "test_Frontend0!")
+        
+        # click login button
+        self.click('input[type="submit"]')
+        
+        # open user profile page
+        self.open(base_url)
+        
+        #validate update form exists
+        self.assert_element("#update-header")
+        self.assert_text("Update a Ticket!", "#update-header")
+        
+        #validate update form includes #name element with correct attached phrase
+        self.assert_element("#name")
+        self.assert_text("Name", "#name")
+        
+        #validate update form includes #quantity element with correct attached phrase
+        self.assert_element("#quantity")
+        self.assert_text("Quantity", "#quantity")
+        
+        #validate update form includes price element with correct attached phrase
+        self.assert_element("#price")
+        self.assert_text("Price", "#price")
+        
+        #validate update form includes date element with correct attached phrase
+        self.assert_element("#date")
+        self.assert_text("Expiration Date", "#date")
+        
+    #test R3.8
+      
+    #test R3.9  
+    @patch('qa327.backend.get_user', return_value=new_test_user)
+    @patch('qa327.backend.get_ticket', return_value=new_test_ticket_1)
+    def test_sell_form(self, *_):
+        
+        # open /logout to invalidate any current logged in sessions
+        self.open(base_url + '/logout')
+        
+        #login page opens
+        self.open(base_url + '/login')
+        
+        #enter email and password of test_user into #email and #password elements
+        self.type("#email", "test_frontend@test.com")
+        self.type("#password", "test_Frontend0!")
+        
+        #click login button
+        self.click('input[type="submit"]')
+        
+        #open user profile page
+        self.open(base_url)
+        
+        #enter ticket name into #sell element
+        self.type("#name", "test_frontend1")
+        
+        #enter quantity into #quantity element
