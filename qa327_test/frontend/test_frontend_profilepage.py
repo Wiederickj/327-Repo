12affbfd0fd84f58a6bfb8f8c0f33812367fffad
HamzaSherik <16hs46@queensu.ca>
2020-11-18 11:39:42 -0500

    removed registration page testing for now to speed up my own testing process. Also, found an error in code resulting from test R3.2 and made the changes to index.html to fix the error

--- qa327_test/frontend/test_frontend_profilepage.py
+++ qa327_test/frontend/test_frontend_profilepage.py
@@ -10,17 +10,43 @@ from werkzeug.security import generate_password_hash, check_password_hash
 This file defines all unit tests for the frontend homepage.
 """
 
-#Sample user
+#sample user
 new_test_user = User(
     email='test_frontend@test.com',
     name='test_frontend',
     password=generate_password_hash('test_Frontend0!')
 )
 
-class LogInPageTestFrontEnd(BaseCase):
+#testing frontend user profile page
+class UserProfilePageTestFrontEnd(BaseCase):
 
     @patch('qa327.backend.get_user', return_value=new_test_user)
+    
+    #test R3.1
     def test_logged_out(self, *_):
 
         # open /logout to invalidate any current logged in sessions
         self.open(base_url + '/logout')
+        
+    #test R3.2
+    def test_hi_header(self, *_):
+        
+        # open /logout to invalidate any current logged in sessions
+        self.open(base_url + '/logout')
+        
+        #enter email and password of test_user into #email and #password elements
+        self.type("#email", "test_frontend@test.com")
+        self.type("#password", "test_Frontend0!")
+        
+        # click login button
+        self.click('input[type="submit"]')
+        
+        # open home page
+        self.open(base_url)
+        
+        #validate  welcome-header element outputs correct phrase
+        self.assert_element("#welcome-header")
+        self.assert_text("Hi test_frontend", "#welcome-header")
+        
+    def #test R3.3
+        
