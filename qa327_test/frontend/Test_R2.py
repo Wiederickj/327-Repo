d3f92d786659f9891673cd3cd17bbb10e4d97839
Gilbert-Chien <gilbertchien99@gmail.com>
2020-11-19 16:00:13 -0500

    added needed imports

--- qa327_test/frontend/Test_R2.py
+++ qa327_test/frontend/Test_R2.py
@@ -1,3 +1,11 @@
+import pytest
+from seleniumbase import BaseCase
+
+from qa327_test.conftest import base_url
+from unittest.mock import patch
+from qa327.models import db, User
+from werkzeug.security import generate_password_hash, check_password_hash
+
 class RegisterTest(BaseCase):
 
     def test_register_page(self, *_):
